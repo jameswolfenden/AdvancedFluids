24c26d43f5f533881a0639f733e16916a8c969cf
JAWB86 <james.jawb86@gmail.com>
2023-03-04 09:46:05 +0000

    automate reading of cell sizes

--- 3D/3dDomain.cpp
+++ 3D/3dDomain.cpp
@@ -44,11 +44,11 @@ void saveWallPressures(std::vector<Domain3D *> domains, std::ofstream &stream)
             {
                 if (i == domains.size() - 1 && z == domains[i]->zFaceCount - 1)
                 {
-                    stream << domains[i]->boxes[0][y][z].p;
+                    stream << domains[i]->boxes[1][y][z].p;
                 }
                 else
                 {
-                    stream << domains[i]->boxes[0][y][z].p << ",";
+                    stream << domains[i]->boxes[1][y][z].p << ",";
                 }
             }
         }
@@ -58,11 +58,11 @@ void saveWallPressures(std::vector<Domain3D *> domains, std::ofstream &stream)
 
 int main()
 {
-    int iterations = 50;
+    int iterations = 500;
     double elapsedTime = 0;
     int domainCount = 10;
     double fridgeHeight = 2.0; // full size
-    // double fridgeHeight = 0.20; // reduced size
+    // double fridgeHeight = 0.40; // reduced size
     double pipeHeight = 0.20;
     double pipeWidth = 0.03;
     double leftFridgeWidth = 0.05;
@@ -73,9 +73,9 @@ int main()
     // double backFridgeDepth = 0.40; // reduced size
     double frontFridgeDepth = (1 - pipeDepth) / 2; // full size
     double backFridgeDepth = (1 - pipeDepth) / 2;  // full size
-    int xCellsPerMetre = 100;
-    int yCellsPerMetre = 100;
-    int zCellsPerMetre = 100;
+    int xCellsPerMetre = 200;
+    int yCellsPerMetre = 200;
+    int zCellsPerMetre = 200;
     int pipeWidthCells = (int)(pipeWidth * xCellsPerMetre) + 2;
     int pipeHeightCells = (int)(pipeHeight * yCellsPerMetre) + 2;
     int leftFridgeWidthCells = (int)(leftFridgeWidth * xCellsPerMetre) + 2;
@@ -114,9 +114,9 @@ int main()
     std::cout << "z: " << z << std::endl;
 
     double fridgePressure = 1.01325;
-    double pipePressure = 1.06;
+    double pipePressure = 1.04;
     double fridgeDensity = 1.268;
-    double pipeDensity = 1.37;
+    double pipeDensity = 1.33;
 
     std::vector<std::vector<bool>> domainsGhosts = {
         // order of sides is top of domain, bottom, left, right, front, back
@@ -250,7 +250,10 @@ int main()
     // save number of iterations to file
     std::ofstream iterationsStream;
     iterationsStream.open("./resultsdomain/iterations.csv");
-    iterationsStream << iterations;
+    iterationsStream << iterations << "\n";
+    iterationsStream << xCellsPerMetre << "\n";
+    iterationsStream << yCellsPerMetre << "\n";
+    iterationsStream << zCellsPerMetre;
     iterationsStream.close();
 
     std::cout << elapsedTime << std::endl; // output elapsed time (of simulation time) to console
--- 3D/wallpressures.jl
+++ 3D/wallpressures.jl
@@ -1,14 +1,17 @@
-
+using DelimitedFiles, Plots
 
 p = readdlm("3D/resultsdomain/wallPressures.csv",',')
-iterations = Int(readdlm("3D/resultsdomain/iterations.csv",',')[1] + 1)
+config = readdlm("3D/resultsdomain/iterations.csv",',')
+iterations = Int(config[1]) + 1
+cellspermetery = Int(config[3])
+cellspermeterz = Int(config[4])
 p = reshape(p, (Int(size(p,1)/iterations), iterations, size(p,2)))
 
 
-toplay = 0:n-2
+toplay = 0:iterations-2
 pWallAnim = Animation()
 for i in toplay
-    pWallPlt = heatmap(p[:,i+1,:], clim=(1.01325,1.017))
+    pWallPlt = heatmap(p[:,i+1,:], clim=(1.013,1.015))
     frame(pWallAnim, pWallPlt)
 end
 mp4(pWallAnim, "3D/resultsdomain/pWall.mp4", fps=30)
@@ -16,8 +19,6 @@ mp4(pWallAnim, "3D/resultsdomain/pWall.mp4", fps=30)
 ATM = 1.01325
 pAbs = p .- ATM
 
-cellspermeterz = 500
-cellspermetery = 200
 cellarea = 1/cellspermetery*1/cellspermeterz
 
 forcecell = pAbs*1e5 .* cellarea
@@ -42,5 +43,18 @@ E = 2.0e11
 t = 0.25e-3
 W = 1.0
 I = t^3 * W / 12
-y = totalforce .* x .* (H^2 .- x.^2).^(3 / 2) / (9 * sqrt(3) * H * E * I)
 
+# find the centre of pressure at each time step
+rowforce = sum(forcecell, dims=3)
+
+# array of the physical x positions of each row
+xs = size(p,1)/cellspermetery.-((1:size(p,1)).-0.5)./cellspermetery
+
+sxsf = dropdims(sum(rowforce.*xs, dims=1), dims=(1,3))
+
+sf = dropdims(sum(rowforce, dims=1), dims=(1,3))
+
+centrepressure = sxsf./ sf
+centrepressure[sf.==0].=0
+
+y = totalforce .* centrepressure .* (H^2 .- centrepressure.^2).^(3 / 2) / (9 * sqrt(3) * H * E * I)
